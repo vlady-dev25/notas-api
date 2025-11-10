3afd80511638b6cb5e3c6bcce0cbbd40f9ad472a
Tavo <tavo@example.com>
2025-11-10 12:28:42 -0600

    Cambiar mensaje de /ping

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/ping")
 def ping():
-    return jsonify({"message": "vladi es el mas venenoso del mundo mundial for ever y crallador"})
+    return jsonify({"message": "vladi es el mas venenoso del mundo mundial for ever y crallador entonces venenus pudiste"})
 
 if __name__ == "__main__":
     app.run(debug=True)
